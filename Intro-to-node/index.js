72d6bd44082d5c2d373536b801656e1e930627cd
KosmasKaisaris <73787322+KosmasKaisaris@users.noreply.github.com>
2022-01-05 02:30:36 +0200

    Add files via upload

---
+++ Intro-to-node/index.js
@@ -0,0 +1,12 @@
+var superheroes = require("superheroes");
+
+var mySuperheroName =superheroes.random();
+
+console.log(mySuperheroName);
+
+
+var supervillains = require("supervillains");
+
+var mySuperVillainName = supervillains.random();
+
+console.log(mySuperVillainName);
